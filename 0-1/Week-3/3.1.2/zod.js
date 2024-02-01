627ca1e8498bd0e29dfaef9306361a777323b67a
Ayush <ayush0999aryan@gmail.com>
2024-02-01 17:33:48 +0530

    zod

--- 0-1/Week-3/3.1.2/zod.js
+++ 0-1/Week-3/3.1.2/zod.js
@@ -1,14 +1,34 @@
-const express = require("express");
+const express = require("express")
+
+const zod = require("zod")
 const app = express();
 
- const zod = require("zod");
  const schema = zod.array(zod.number());
 
 app.use(express.json());
 
-app.post("/health-checkup ",(req,res)=>{
+app.post("/health-checkup1 ", function(req,res){
     // kidneys = [1,2]
 
     const kidneys = req.body.kidneys;
+    const response = schema.safeParse(kidneys)
+    if(!response.success){
+        res.status(411).json({
+            msg:"input is invalid"
+        })
+    }
+    else{
+        res.json({
+          msg:"valid"
+        })       
+    }
+});
 
-})
+  
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+app.listen(3000, ()=>{
+    console.log(`Your port started at 3000`)
+});
